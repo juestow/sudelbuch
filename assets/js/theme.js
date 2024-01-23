6b3c994a7c67c05038bb60f9b23fa79b26e21ab7
juestow <111063082+juestow@users.noreply.github.com>
2024-01-24 00:09:20 +0100

    Add files via upload

---
+++ assets/js/theme.js
@@ -0,0 +1,17 @@
+const themeMap = {
+  'default': {
+    'background-color': '#292d3e',
+    'text-color': '#fffff8',
+    'highlight-color': '#2e2e2e'
+  },
+  'solarized': {
+    'background-color': '#fdf6e3',
+    'text-color': '#222',
+    'highlight-color': '#eee'
+  },
+  'grey': {
+    'background-color': '#dcdcdc',
+    'text-color': '#000080',
+    'highlight-color': '#2e2e2e'
+  }
+}
